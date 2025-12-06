2bb0deda3e1bfa833cd1cd1e0558cb616dd09bf5
Mohammed20201991 <mohamedabid092@gmail.com>
2025-12-06 13:15:18 +0300

    Lab-2-Coding

---
+++ Labs/Lab-2/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+int main() {
+ string name;
+ // note the postal code here is string type not string
+ string address;
+ cout << "Enter your name: ";
+ cin >> name;
+ cout << "Enter your address: ";
+ cin >> address;
+ cout << "Hello " << name << ", you are living at" << address <<".";
+ return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    cout << "Hello world!" << endl;
+//        cout << "Hello world!" << endl;
+//
+//    return 0;
+//}
